6bb2b1ec3c438ad79a9ce39fc6697b14bcca4aad
gunal15 <gunalnandakumar@gmail.com>
2023-04-10 11:22:45 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+import Dashboard from './Dashboard';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+} from "react-router-dom";
+import Users from './Users';
+import Createuser from './Createuser';
+import Edituser from './Edituser';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div id="wrapper">
+        <Sidebar></Sidebar>
+        <div id="content-wrapper" className="d-flex flex-column">
+          <div id="content">
+            <Topbar></Topbar>
+            <div class="container-fluid">
+              <Routes>
+                <Route path='/' element={<Dashboard />}  exact={true}></Route>
+                <Route path='/users' element={<Users></Users>} exact={true}></Route>
+                <Route path='/users-create' element={<Createuser></Createuser>} exact={true}></Route>
+                <Route path='/users-edit/:id' element={<Edituser />} />
+                  </Routes>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/Edituser.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Edituser(props) {
+  console.log(props)
+
+  return (
+    <div>
+      User Edit -
+    </div>
+  )
+}
+export default Edituser
